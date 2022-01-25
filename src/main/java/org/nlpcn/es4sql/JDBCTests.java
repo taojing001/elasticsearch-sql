a9750a00fc55128e62a78ee5da381b409f554397
taojing <taojing@easemob.com>
2022-01-25 11:46:51 +0800

    添加EsDriver

---
+++ src/main/java/org/nlpcn/es4sql/JDBCTests.java
@@ -0,0 +1,29 @@
+package org.nlpcn.es4sql;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.ElasticSearchDruidDataSourceFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Properties;
+
+public class JDBCTests {
+
+    public static void main(String[] args) throws Exception {
+        Properties properties = new Properties();
+        properties.put("url", "jdbc:elasticsearch://127.0.0.1:9300");
+        DruidDataSource dds = (DruidDataSource) ElasticSearchDruidDataSourceFactory.createDataSource(properties);
+        dds.setInitialSize(1);
+        Connection connection = dds.getConnection();
+        PreparedStatement ps = connection.prepareStatement("select count(1),avg(tenantId) as sessionCount from session_survey group by tenantId,channelId,date_histogram(field='createTime','interval'='1d')");
+        ResultSet resultSet = ps.executeQuery();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getDouble("sessionCount"));
+        }
+
+        ps.close();
+        connection.close();
+        dds.close();
+    }
+}
---
+++ src/main/java/org/nlpcn/es4sql/jdbc/EsDriver.java
@@ -0,0 +1,72 @@
+package org.nlpcn.es4sql.jdbc;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.ElasticSearchDruidDataSourceFactory;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class EsDriver implements Driver {
+
+    static
+    {
+        try {
+            DriverManager.registerDriver(new EsDriver());
+        } catch (SQLException e) {
+            // TODO 自动生成的 catch 块
+            e.printStackTrace();
+        }
+
+    }
+
+    public Connection connect(String url, Properties info) throws SQLException {
+        Properties properties = new Properties();
+        properties.put("url", url);
+        DruidDataSource dds=null;
+
+        try {
+            dds = (DruidDataSource) ElasticSearchDruidDataSourceFactory.createDataSource(properties);
+        } catch (Exception e) {
+            // TODO 自动生成的 catch 块
+            e.printStackTrace();
+        }
+
+        assert dds != null;
+        dds.setInitialSize(1);
+        return dds.getConnection();
+    }
+
+    public boolean acceptsURL(String url) throws SQLException {
+        // TODO 自动生成的方法存根
+        if (!url.startsWith("jdbc:elasticsearch:"))
+            return false;
+        else
+            return true;
+    }
+
+    public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+        // TODO 自动生成的方法存根
+        return null;
+    }
+
+    public int getMajorVersion() {
+        // TODO 自动生成的方法存根
+        return 0;
+    }
+
+    public int getMinorVersion() {
+        // TODO 自动生成的方法存根
+        return 0;
+    }
+
+    public boolean jdbcCompliant() {
+        // TODO 自动生成的方法存根
+        return false;
+    }
+
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        // TODO 自动生成的方法存根
+        return null;
+    }
+}
